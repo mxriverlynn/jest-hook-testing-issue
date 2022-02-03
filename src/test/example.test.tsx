feb89ccea4280cc0203981c8d6c370fd1d1af4a8
River Lynn Bailey <riverlynn.bailey@gusto.com>
2022-02-03 11:13:29 -0600

    updated to show solution

--- src/test/example.test.tsx
+++ src/test/example.test.tsx
@@ -1,16 +1,12 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import { MigrationActions } from './migration_store';
-
 import useSectionDisplayRules from './example';
+import useStore from './useStore';
 
 jest.mock('./useStore', () => {
   return {
     __esModule: true,
-    default: jest.fn(() => {
-      return {
-        toJS() {},
-      };
-    }),
+    default: jest.fn(() => {}),
   };
 });
 
@@ -24,32 +20,131 @@ jest.mock('./migration_store', () => {
 });
 
 describe('useSectionDisplayRules', () => {
+  let actionPromise: any;
   let fetchComplete: any;
-  const sectionName = 'section_schedule_your_intro_call';
+  let sectionKey: string;
+  let payrollProvider: string;
+  let accessMethod: string;
 
   beforeEach(() => {
-    const actionPromise = new Promise((r) => {
-      fetchComplete = r;
+    sectionKey = 'some_random_section_key';
+    payrollProvider = 'some random provider';
+    accessMethod = 'share_access';
+  });
+
+  const resolveDataLoading = async () => {
+    fetchComplete();
+    return actionPromise;
+  };
+
+  const runHook = async (options: { resolve: boolean } = {}) => {
+    actionPromise = new Promise(resolve => {
+      fetchComplete = resolve;
     });
 
-    MigrationActions.fetch.mockImplementation(() => {
-      return actionPromise;
+    MigrationActions.fetch.mockImplementation(() => actionPromise);
+
+    useStore.mockImplementation(() => {
+      return {
+        toJS() {
+          return {
+            previous_payroll_provider: payrollProvider,
+            how_do_you_want_us_to_sign_in_to_your_account: accessMethod,
+          };
+        },
+      };
+    });
+
+    const displayRules = renderHook(() => useSectionDisplayRules());
+
+    if (options.resolve) {
+      await act(resolveDataLoading);
+    }
+
+    return displayRules;
+  };
+
+  describe('loading', () => {
+    it('cannot show any sections until loading is complete', async () => {
+      const { result } = await runHook();
+
+      expect(result.current.loading).toBe(true);
+      expect(result.current.canShow(sectionKey)).toBe(false);
+
+      await act(resolveDataLoading);
+
+      expect(result.current.loading).toBe(false);
+      expect(result.current.canShow(sectionKey)).toBe(true);
     });
   });
 
-  describe('loading', () => {
-    it('cannot show any sections until loading is complete', () => {
-      const { result } = renderHook(() => useSectionDisplayRules());
+  describe('schedule intro call section check', () => {
+    beforeEach(() => {
+      sectionKey = 'section_schedule_your_intro_call';
+    });
 
-      expect(result.current.loading).toBe(true);
-      expect(result.current.canShow(sectionName)).toBe(false);
-
-      act(() => {
-        fetchComplete();
+    describe('when previous provider is QuickBooks Online', () => {
+      beforeEach(() => {
+        payrollProvider = 'QuickBooks Online';
       });
 
-      expect(result.current.loading).toBe(false);
-      expect(result.current.canShow(sectionName)).toBe(true);
+      describe('access method includes credentials', () => {
+        beforeEach(() => {
+          accessMethod = 'share_access';
+        });
+
+        it('can show the section', async () => {
+          const { result } = await runHook({ resolve: true });
+
+          expect(result.current.loading).toBe(false);
+          expect(result.current.canShow(sectionKey)).toBe(true);
+        });
+      });
+
+      describe('and access method is set to add gusto as an admin', () => {
+        beforeEach(() => {
+          accessMethod = 'admin';
+        });
+
+        it('cannot show the section', async () => {
+          const { result } = await runHook({ resolve: true });
+
+          expect(result.current.loading).toBe(false);
+          expect(result.current.canShow(sectionKey)).toBe(false);
+        });
+      });
+    });
+
+    describe('when previous provider is Intuit Online Payroll', () => {
+      beforeEach(() => {
+        payrollProvider = 'Intuit Online Payroll';
+      });
+
+      describe('access method includes credentials', () => {
+        beforeEach(() => {
+          accessMethod = 'share_access';
+        });
+
+        it('can show the section', async () => {
+          const { result } = await runHook({ resolve: true });
+
+          expect(result.current.loading).toBe(false);
+          expect(result.current.canShow(sectionKey)).toBe(true);
+        });
+      });
+
+      describe('and access method is set to add gusto as an admin', () => {
+        beforeEach(() => {
+          accessMethod = 'admin';
+        });
+
+        it('cannot show the section', async () => {
+          const { result } = await runHook({ resolve: true });
+
+          expect(result.current.loading).toBe(false);
+          expect(result.current.canShow(sectionKey)).toBe(false);
+        });
+      });
     });
   });
 });
--- src/test/example.tsx
+++ src/test/example.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import useStore from './useStore';
 import { MigrationStore, MigrationActions } from './migration_store';
 
+const providers = ['QuickBooks Online', 'Intuit Online Payroll'];
+
 const useSectionDisplayRules = () => {
   const migration = useStore(MigrationStore);
   const migrationData = migration.toJS();
@@ -13,8 +15,19 @@ const useSectionDisplayRules = () => {
     });
   }, []);
 
-  const canShow = section_key => {
-    if (loading) { return false; }
+  const canShow = (section_key: string) => {
+    if (loading) {
+      return false;
+    }
+
+    const isIntroCall = section_key === 'section_schedule_your_intro_call';
+    const isProvider = providers.includes(migrationData.previous_payroll_provider);
+    const isInvite = migrationData.how_do_you_want_us_to_sign_in_to_your_account === 'admin';
+
+    if (isIntroCall && isProvider && isInvite) {
+      return false;
+    }
+
     return true;
   };
 
